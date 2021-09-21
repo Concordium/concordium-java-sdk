a0a43e2c281ae73dfe7a55f5c2389fe0b1f0e1f7
Emil Lai <el@concordium.com>
2021-09-21 13:38:49 +0200

    Removed the old test

--- concordium-sdk/src/test/java/com/concordium/sdk/transactions/TransferWithMemoTransactionTest.java
+++ concordium-sdk/src/test/java/com/concordium/sdk/transactions/TransferWithMemoTransactionTest.java
@@ -19,31 +19,6 @@ public class TransferWithMemoTransactionTest {
         try {
             val transaction = TransferWithMemoTransaction
                     .builder()
-                    .memo(Memo.from("6b48656c6c6f20576f726c64"))
-                    .sender(AccountAddress.from("3a3gm7Bd2xDvRN6eMfHDXWXHyxuDaq3CQGEAXiXRTMi7pV4rax"))
-                    .receiver(AccountAddress.from("3YeRQYpKSvDRsH8hWY6KTJ6TJuYxM6MUEYW4wCVZ1je4UA8ar9"))
-                    .amount(GTUAmount.fromMicro(1000000))
-                    .nonce(AccountNonce.from(1))
-                    .expiry(Expiry.from(1632131765))
-                    .signer(TransactionSigner.from(
-                            SignerEntry.from(Index.from(0), Index.from(0),
-                                    ED25519SecretKey.from("b427f9ee55c966805dac37216864348452d42322a25b7ed8b7c56f7926b2e111")),
-                            SignerEntry.from(Index.from(0), Index.from(1),
-                                    ED25519SecretKey.from("69ab1478b950175720a199846e114d5a25de0ddca8539744be680cf3702e695a")),
-                            SignerEntry.from(Index.from(0), Index.from(2),
-                                    ED25519SecretKey.from("b8ae248d6a18bcd40187f255e9f0ef3e593190a371f7c0fa6ff6814c71b066f7"))))
-                    .build();
-            assertEquals("dfee5f4aede94657790cc314474c9caff76f836a99cef9ad6688c5644c36d8b0", transaction.getHash().asHex());
-        } catch (TransactionCreationException e) {
-            fail("Unexpected error: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testCreateTransferWithMemoTwo() {
-        try {
-            val transaction = TransferWithMemoTransaction
-                    .builder()
                     .memo(Memo.from(new byte[]{1, 2, 3, 4, 5}))
                     .sender(AccountAddress.from("3JwD2Wm3nMbsowCwb1iGEpnt47UQgdrtnq2qT6opJc3z2AgCrc"))
                     .receiver(AccountAddress.from("3hYXYEPuGyhFcVRhSk2cVgKBhzVcAryjPskYk4SecpwGnoHhuM"))
