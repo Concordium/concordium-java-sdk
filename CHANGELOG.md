0b10278729fd9ceb67d9405ae5c8c7ea1351688b
Emil Lai <39825288+EmilLa1@users.noreply.github.com>
2022-04-08 14:56:38 +0200

    Support for account alias (#18)
    * Support for account alias

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## Unreleased changes
+
+## 1.1.0
+- Support for account alias.
--- concordium-sdk/src/main/java/com/concordium/sdk/transactions/AccountAddress.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/transactions/AccountAddress.java
@@ -1,16 +1,22 @@
 package com.concordium.sdk.transactions;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.val;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Objects;
 
 @ToString
+@EqualsAndHashCode
 public final class AccountAddress {
     public static final int BYTES = 32;
     private final static int VERSION = 1;
+    private final static int ACCOUNT_ADDRESS_PREFIX_SIZE = 29;
+    private final static int ALIAS_MAX_VALUE = (2 << 23); // 2^24
 
     @Getter
     private final byte[] bytes;
@@ -30,6 +36,25 @@ public final class AccountAddress {
     public static AccountAddress from(String address) {
         val addressBytes = Base58.decodeChecked(VERSION, address);
         return AccountAddress.from(addressBytes);
+    }
+
+    public AccountAddress newAlias(int alias) {
+        if (alias < 0) {
+            throw new NumberFormatException("Alias must be non negative.");
+        }
+        if (alias > ALIAS_MAX_VALUE) {
+            throw new IllegalArgumentException("Alias too large, the provided alias must not be larger than 2^24.");
+        }
+        val buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.putInt(alias);
+        val newAlias = this.bytes.clone();
+        System.arraycopy(buffer.array(), 1, newAlias, ACCOUNT_ADDRESS_PREFIX_SIZE, 3);
+        return AccountAddress.from(newAlias);
+    }
+
+    public boolean isAliasOf(AccountAddress other) {
+        return Arrays.equals(Arrays.copyOfRange(this.bytes, 0, ACCOUNT_ADDRESS_PREFIX_SIZE),
+                Arrays.copyOfRange(other.bytes, 0, ACCOUNT_ADDRESS_PREFIX_SIZE));
     }
 
     public static AccountAddress from(byte[] addressBytes) {
