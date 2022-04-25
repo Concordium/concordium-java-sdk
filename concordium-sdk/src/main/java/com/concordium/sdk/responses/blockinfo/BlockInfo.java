8f6b91e8a64ba158571c8524f4cb96a2047d165c
Aleš Bizjak <abizjak@users.noreply.github.com>
2022-04-25 08:36:17 +0200

    Fix formatting of doc comments.
    Co-authored-by: Emil Lai <39825288+EmilLa1@users.noreply.github.com>

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blockinfo/BlockInfo.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blockinfo/BlockInfo.java
@@ -16,60 +16,60 @@ import java.time.OffsetDateTime;
 @Getter
 public class BlockInfo {
     /**
-     * / Hash of the block.
+     *  Hash of the block.
      */
     private Hash blockHash;
     /**
-     * / The total energy consumption of transactions in the block.
+     *  The total energy consumption of transactions in the block.
      */
     private Integer transactionEnergyCost;
     /**
-     * / Identity of the baker of the block. For non-genesis blocks the value is
-     * / non-null.
+     *  Identity of the baker of the block. For non-genesis blocks the value is
+     *  non-null.
      */
     private Integer blockBaker;
     /**
-     * / Hash of the block state at the end of the given block.
+     *  Hash of the block state at the end of the given block.
      */
     private Hash blockStateHash;
     /**
-     * / Slot time of the slot the block is in. In contrast to
-     * / {@link BlockInfo#blockArriveTime} this is an objective value, all nodes
-     * / agree on it.
+     *  Slot time of the slot the block is in. In contrast to
+     *  {@link BlockInfo#blockArriveTime} this is an objective value, all nodes
+     *  agree on it.
      */
     private OffsetDateTime blockSlotTime;
     /**
-     * / Parent block pointer.
+     *  Parent block pointer.
      */
     private Hash blockParent;
     /**
-     * / Time when the block was first received by the node. This can be in
-     * / principle quite different from the arrive time if, e.g., block execution
-     * / takes a long time, or the block must wait for the arrival of its parent.
+     *  Time when the block was first received by the node. This can be in
+     *  principle quite different from the arrive time if, e.g., block execution
+     *  takes a long time, or the block must wait for the arrival of its parent.
      */
     private OffsetDateTime blockReceiveTime;
     /**
-     * / The genesis index for this block. This counts the number of protocol
-     * / updates that have preceded this block, and defines the era of the
-     * / block.
+     *  The genesis index for this block. This counts the number of protocol
+     *  updates that have preceded this block, and defines the era of the
+     *  block.
      */
     private Integer genesisIndex;
     /**
-     * / Slot number of the slot the block is in.
+     *  Slot number of the slot the block is in.
      */
     private Integer blockSlot;
     /**
-     * / Whether the block is finalized or not.
+     *  Whether the block is finalized or not.
      */
     private Boolean finalized;
     /**
-     * / The height of this block relative to the (re)genesis block of its era.
+     *  The height of this block relative to the (re)genesis block of its era.
      */
     private Integer eraBlockHeight;
     /**
-     * / Pointer to the last finalized block. Each block has a pointer to a
-     * / specific finalized block that existed at the time the block was
-     * / produced.
+     *  Pointer to the last finalized block. Each block has a pointer to a
+     *  specific finalized block that existed at the time the block was
+     *  produced.
      */
     private Hash blockLastFinalized;
     /**
@@ -77,11 +77,11 @@ public class BlockInfo {
      */
     private Integer transactionsSize;
     /**
-     * / Height of the block from genesis.
+     *  Height of the block from genesis.
      */
     private Integer blockHeight;
     /**
-     * / The number of transactions in the block.
+     *  The number of transactions in the block.
      */
     private Integer transactionCount;
     /**
