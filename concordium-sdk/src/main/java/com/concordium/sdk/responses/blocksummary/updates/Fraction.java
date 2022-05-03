24582d3b2b593388d44d670bc4f453259f745931
Emil Lai <el@concordium.com>
2022-05-03 10:45:58 +0200

    .

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/updates/Fraction.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/updates/Fraction.java
@@ -6,15 +6,17 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.math.BigInteger;
+
 @ToString
 @EqualsAndHashCode
 @Getter
 public class Fraction {
-    private final int denominator;
-    private final int numerator;
+    private final BigInteger denominator;
+    private final BigInteger numerator;
 
     @JsonCreator
-    Fraction(@JsonProperty("denominator") int denominator, @JsonProperty("numerator") int numerator) {
+    Fraction(@JsonProperty("denominator") BigInteger denominator, @JsonProperty("numerator") BigInteger numerator) {
         this.denominator = denominator;
         this.numerator = numerator;
     }
