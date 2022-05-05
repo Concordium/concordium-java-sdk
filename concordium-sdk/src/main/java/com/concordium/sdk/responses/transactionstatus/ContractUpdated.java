ea55729a25c15e6453ac3a2b504e8fa225650625
Emil Lai <mail@emillai.com>
2022-05-05 16:08:46 +0200

    Doc for contract `Updated` event

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/transactionstatus/ContractUpdated.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/transactionstatus/ContractUpdated.java
@@ -10,15 +10,47 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * A contract was updated
+ */
 @ToString
 @Getter
 public class ContractUpdated extends TransactionResultEvent {
+
+    /**
+     * The amount provided
+     */
     private CCDAmount amount;
+
+    /**
+     * The instigator i.e. the source invoking the contract.
+     */
     private final AbstractAccount instigator;
+
+    /**
+     * The address of the contract
+     */
     private final ContractAddress address;
+
+    /**
+     * The name of the `receive` function which was called.
+     */
     private final String receiveName;
+
+    /**
+     * Events as reported by the contract via the log method, in the
+     * order they were reported.
+     */
     private final List<String> events;
+
+    /**
+     * The message which was sent to the contract.
+     */
     private final String message;
+
+    /**
+     * The contract version
+     */
     private final int version;
 
     @JsonCreator
@@ -28,7 +60,7 @@ public class ContractUpdated extends TransactionResultEvent {
                     @JsonProperty("receiveName") String receiveName,
                     @JsonProperty("events") List<String> events,
                     @JsonProperty("message") String message,
-                    @JsonProperty("version") int version) {
+                    @JsonProperty("contractVersion") int version) {
         this.instigator = AbstractAccount.parseAccount(instigator);
         this.address = (ContractAddress) AbstractAccount.parseAccount(address);
         this.receiveName = receiveName;
