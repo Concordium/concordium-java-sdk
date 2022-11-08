6a34ef7a2a91e7f93f00dba20635e73786745af3
tanmay <tanmaydeepsharma21@gmail.com>
2022-11-08 10:38:08 +0530

    Add documentation to Parameter class

--- concordium-sdk/src/main/java/com/concordium/sdk/transactions/Parameter.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/transactions/Parameter.java
@@ -10,7 +10,10 @@ import java.nio.ByteBuffer;
 
 
 /**
+ * The parameters are used for updating the smart contract instance.
+ * i.e. calling a recieve function exposed in the smart contract with the parameters.
  * Buffer of the parameters message.
+ * In the current supported protocols the size is limited to be 1kb.
  */
 
 @Getter
@@ -29,7 +32,6 @@ public final class Parameter {
     public static Parameter from(byte[] parameter) {
         return new Parameter(parameter);
     }
-
 
     /**
      * @return buffer bytes of {@link Parameter}.
