870248e7fee56df22eac5b5a5b541889a88f040d
parv <parv0888@gmail.com>
2023-02-19 23:24:44 +0530

    fixed block info and block identifier relation

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blockinfo/BlockInfo.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blockinfo/BlockInfo.java
@@ -3,19 +3,27 @@ package com.concordium.sdk.responses.blockinfo;
 import com.concordium.sdk.responses.BlockIdentifier;
 import com.concordium.sdk.serializing.JsonMapper;
 import com.concordium.sdk.transactions.Hash;
+import com.concordium.sdk.types.UInt64;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.Getter;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
-import lombok.extern.jackson.Jacksonized;
+import lombok.*;
 
 import java.time.OffsetDateTime;
 
 @ToString
 @Getter
-@SuperBuilder
-@Jacksonized
-public class BlockInfo extends BlockIdentifier {
+@EqualsAndHashCode
+public class BlockInfo {
+
+    /**
+     * Block Identifiers, Block Height & Block Hash
+     */
+    @JsonIgnore
+    @Getter(AccessLevel.NONE)
+    private final BlockIdentifier blockIdentifier;
+
     /**
      * The total energy consumption of transactions in the block.
      */
@@ -83,6 +91,61 @@ public class BlockInfo extends BlockIdentifier {
      * (i.e., node specific) value.
      */
     private final OffsetDateTime blockArriveTime;
+
+    /**
+     * Hash of the Block.
+     */
+    @JsonProperty("blockHash")
+    public Hash getBlockHash() {
+        return this.blockIdentifier.getBlockHash();
+    }
+
+    /**
+     * Absolute Block Height.
+     */
+    @JsonProperty("blockHeight")
+    public UInt64 getBlockHeight() {
+        return blockIdentifier.getBlockHeight();
+    }
+
+    @Builder
+    @JsonCreator
+    BlockInfo(
+            @JsonProperty("blockHash") final Hash blockHash,
+            @JsonProperty("blockHeight") final UInt64 blockHeight,
+            @JsonProperty("transactionEnergyCost") final Integer transactionEnergyCost,
+            @JsonProperty("blockBaker") final Integer blockBaker,
+            @JsonProperty("blockStateHash") final Hash blockStateHash,
+            @JsonProperty("blockSlotTime") final OffsetDateTime blockSlotTime,
+            @JsonProperty("blockParent") final Hash blockParent,
+            @JsonProperty("blockReceiveTime") final OffsetDateTime blockReceiveTime,
+            @JsonProperty("genesisIndex") final Integer genesisIndex,
+            @JsonProperty("blockSlot") final Integer blockSlot,
+            @JsonProperty("finalized") final Boolean finalized,
+            @JsonProperty("eraBlockHeight") final Integer eraBlockHeight,
+            @JsonProperty("blockLastFinalized") final Hash blockLastFinalized,
+            @JsonProperty("transactionsSize") final Integer transactionsSize,
+            @JsonProperty("transactionCount") final Integer transactionCount,
+            @JsonProperty("blockArriveTime") final OffsetDateTime blockArriveTime) {
+        this.blockIdentifier = BlockIdentifier.builder()
+                .blockHash(blockHash)
+                .blockHeight(blockHeight)
+                .build();
+        this.transactionEnergyCost = transactionEnergyCost;
+        this.blockBaker = blockBaker;
+        this.blockStateHash = blockStateHash;
+        this.blockSlotTime = blockSlotTime;
+        this.blockParent = blockParent;
+        this.blockReceiveTime = blockReceiveTime;
+        this.genesisIndex = genesisIndex;
+        this.blockSlot = blockSlot;
+        this.finalized = finalized;
+        this.eraBlockHeight = eraBlockHeight;
+        this.blockLastFinalized = blockLastFinalized;
+        this.transactionsSize = transactionsSize;
+        this.transactionCount = transactionCount;
+        this.blockArriveTime = blockArriveTime;
+    }
 
     public static BlockInfo fromJson(String blockInfoJsonString) {
         try {
