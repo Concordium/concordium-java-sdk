14df7b7acba6eda28a0b3b384233a32020f81599
tds-1 <32020192+tds-1@users.noreply.github.com>
2023-04-17 15:13:53 +0530

    GRPC V2 : Get Block Item Status (#214)
    Co-authored-by: Parv Sharma <parv@stactrace.com>
    Co-authored-by: Emil Lai <el@concordium.com>

--- concordium-sdk/src/main/java/com/concordium/sdk/ClientV2.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/ClientV2.java
@@ -5,6 +5,7 @@ import com.concordium.grpc.v2.Empty;
 import com.concordium.grpc.v2.QueriesGrpc;
 import com.concordium.sdk.exceptions.BlockNotFoundException;
 import com.concordium.sdk.exceptions.ClientInitializationException;
+import com.concordium.sdk.exceptions.TransactionNotFoundException;
 import com.concordium.sdk.requests.BlockHashInput;
 import com.concordium.sdk.requests.getaccountinfo.AccountRequest;
 import com.concordium.sdk.responses.BlockIdentifier;
@@ -12,10 +13,12 @@ import com.concordium.sdk.responses.accountinfo.AccountInfo;
 import com.concordium.sdk.responses.blockinfo.BlockInfo;
 import com.concordium.sdk.responses.blocksummary.updates.queues.AnonymityRevokerInfo;
 import com.concordium.sdk.responses.consensusstatus.ConsensusStatus;
+import com.concordium.sdk.responses.transactionstatus.TransactionStatus;
 import com.concordium.sdk.transactions.AccountAddress;
 import com.concordium.sdk.transactions.AccountNonce;
 import com.concordium.sdk.transactions.Transaction;
 import com.concordium.sdk.transactions.BlockItem;
+import com.concordium.sdk.transactions.Hash;
 import io.grpc.CallCredentials;
 import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
@@ -27,6 +30,7 @@ import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 import static com.concordium.sdk.ClientV2MapperExtensions.to;
+import static com.concordium.sdk.ClientV2MapperExtensions.toTransactionHash;
 
 /**
  * The Client is responsible for sending requests to the node.
@@ -196,6 +200,23 @@ public final class ClientV2 {
         var grpcOutput = this.server()
                 .getNextAccountSequenceNumber(to(address));
         return to(grpcOutput);
+    }
+
+    /**
+     * Retrieves the {@link TransactionStatus} for a given transaction {@link Hash}
+     *
+     * @param transactionHash The transaction {@link Hash}
+     * @return The {@link TransactionStatus}
+     * @throws {@link BlockNotFoundException} if the transaction was not found.
+     */
+    public TransactionStatus getBlockItemStatus(Hash transactionHash) throws BlockNotFoundException {
+        try {
+            var grpcOutput = this.server()
+                    .getBlockItemStatus(toTransactionHash(transactionHash));
+            return to(grpcOutput);
+        } catch (StatusRuntimeException e) {
+            throw BlockNotFoundException.from(transactionHash);
+        }
     }
 
     /**
