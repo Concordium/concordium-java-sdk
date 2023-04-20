f5ab43bc101a7d7f6e6ec0d2a21dee46dfa6a5c2
magnusbechwind <magnusbechwind@gmail.com>
2023-04-20 11:24:38 +0200

    added example for banning and unbanning a peer

---
+++ concordium-sdk-examples/src/main/java/com/concordium/sdk/examples/BanPeer.java
@@ -0,0 +1,41 @@
+package com.concordium.sdk.examples;
+
+import com.concordium.sdk.ClientV2;
+import com.concordium.sdk.Connection;
+import com.concordium.sdk.Credentials;
+import picocli.CommandLine;
+
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "BanPeer", mixinStandardHelpOptions = true)
+public class BanPeer implements Callable<Integer> {
+
+    @CommandLine.Option(
+            names = {"--endpoint"},
+            description = "GRPC interface of the node.",
+            defaultValue = "http://localhost:20001")
+    private String endpoint;
+
+    @Override
+    public Integer call() throws Exception {
+        URL endpointUrl = new URL(this.endpoint);
+        Connection connection = Connection.builder()
+                .host(endpointUrl.getHost())
+                .port(endpointUrl.getPort())
+                .credentials(new Credentials())
+                .build();
+
+        InetAddress peerToBan = InetAddress.getByName("3.97.143.216");
+        ClientV2
+                .from(connection)
+                .banPeer(peerToBan);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new BanPeer()).execute(args);
+        System.exit(exitCode);
+    }
+}
