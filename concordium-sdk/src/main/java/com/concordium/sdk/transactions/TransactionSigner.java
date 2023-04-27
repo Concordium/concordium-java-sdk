744a853aaec60338c8135bf15e0f412b1d27a90e
magnusbechwind <magnusbechwind@gmail.com>
2023-04-27 10:17:56 +0200

    Implemented importing wallet keys from chrome plugin export

--- concordium-sdk/src/main/java/com/concordium/sdk/transactions/TransactionSigner.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/transactions/TransactionSigner.java
@@ -1,7 +1,13 @@
 package com.concordium.sdk.transactions;
 
+import com.concordium.sdk.crypto.ed25519.ED25519SecretKey;
 import com.concordium.sdk.exceptions.ED25519Exception;
+import com.concordium.sdk.serializing.JsonMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.val;
+
+import java.io.File;
+import java.io.IOException;
 
 public interface TransactionSigner {
 
@@ -31,6 +37,32 @@ public interface TransactionSigner {
         val transactionSigner = new TransactionSignerImpl();
         for (SignerEntry entry : entries) {
             transactionSigner.put(entry.getCredentialIndex(), entry.getKeyIndex(), entry.getSigner());
+        }
+        return transactionSigner;
+    }
+
+    /**
+     * Creates a new {@link TransactionSignerImpl} from {@link File} containing exported wallet
+     *
+     * @param walletExport The {@link File} containing an exported wallet
+     * @return the {@link TransactionSignerImpl} populated with keys from the exported wallet
+     * @throws IOException //TODO
+     */
+    static TransactionSignerImpl from(File walletExport) throws IOException {
+        val transactionSigner = new TransactionSignerImpl();
+        JsonNode json = JsonMapper.INSTANCE.readTree(walletExport);
+        val inputEntries = json.get("value").get("accountKeys").get("keys");
+        int nrOfEntries = inputEntries.size();
+        for (int i = 0; i < nrOfEntries; i++) {
+            val credentialIndex = Index.from(i);
+            val entryKeys = inputEntries.get(String.valueOf(i)).get("keys");
+            int nrOfKeys = entryKeys.size();
+            for (int j = 0; j < nrOfKeys; j++) {
+                val keyIndex = Index.from(j);
+                val signKey = entryKeys.get(String.valueOf(j)).get("signKey").asText();
+                val signer = ED25519SecretKey.from(signKey);
+                transactionSigner.put(credentialIndex, keyIndex, signer);
+            }
         }
         return transactionSigner;
     }
