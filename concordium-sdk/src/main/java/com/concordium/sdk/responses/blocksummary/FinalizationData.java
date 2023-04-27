f02e47ecdf9a6c3c975681898902510950c8e0ea
magnusbechwind <magnusbechwind@gmail.com>
2023-04-27 14:52:16 +0200

    Added unit test for getBlockFinalizationSummary

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/FinalizationData.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/FinalizationData.java
@@ -3,6 +3,7 @@ package com.concordium.sdk.responses.blocksummary;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @ToString
 @Builder
+@EqualsAndHashCode
 public final class FinalizationData {
     private final String finalizationBlockPointer;
     private final int finalizationIndex;
--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/Finalizer.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/Finalizer.java
@@ -4,6 +4,7 @@ import com.concordium.sdk.responses.AccountIndex;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -15,6 +16,7 @@ import java.math.BigInteger;
 @Getter
 @ToString
 @Builder
+@EqualsAndHashCode
 public final class Finalizer {
     /**
      * The baker id
