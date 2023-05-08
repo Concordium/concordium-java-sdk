a15a69b6bd4101f49e6e1344ecd7a0572101df95
magnusbechwind <magnusbechwind@gmail.com>
2023-05-08 10:47:55 +0200

    Cleanup

--- concordium-sdk-examples/src/main/java/com/concordium/sdk/examples/GetBlockFinalizationSummary.java
+++ concordium-sdk-examples/src/main/java/com/concordium/sdk/examples/GetBlockFinalizationSummary.java
@@ -14,7 +14,7 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 
-/**  TODO
+/**
  * Creates a {@link ClientV2} from the specified connection ("http://localhost:20001" if not specified).
  * Retrieves and prints the {@link FinalizationData} for the block {@link BlockHashInput#BEST}.
  */
