09c4a16a865d4c2453e7bebf5e1aae4d47f52662
Emil Lai <39825288+EmilLa1@users.noreply.github.com>
2023-07-31 09:54:49 +0200

    Add example for GetBlockTransactionEvents (#254)
    * Add example for GetBlockTransactionEvents.

--- concordium-sdk-examples/src/main/java/com/concordium/sdk/examples/GetBlockTransactionEvents.java
+++ concordium-sdk-examples/src/main/java/com/concordium/sdk/examples/GetBlockTransactionEvents.java
@@ -4,32 +4,25 @@ import com.concordium.sdk.ClientV2;
 import com.concordium.sdk.Connection;
 import com.concordium.sdk.exceptions.ClientInitializationException;
 import com.concordium.sdk.requests.BlockQuery;
-import com.concordium.sdk.responses.modulelist.ModuleRef;
-import com.concordium.sdk.transactions.smartcontracts.WasmModule;
+import com.concordium.sdk.responses.blockitemsummary.Summary;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 /**
- * Gets the source of the module specified by {@link GetModuleSource#moduleRef} and prints it to the console.
+ * Gets the block transaction events by {@link com.concordium.sdk.examples.GetBlockTransactionEvents} and prints it to the console.
  */
-@Command(name = "GetModuleSource", mixinStandardHelpOptions = true)
-public class GetModuleSource implements Callable<Integer> {
-    @Option(
+@CommandLine.Command(name = "GetBlockTransactionEvents", mixinStandardHelpOptions = true)
+public class GetBlockTransactionEvents implements Callable<Integer> {
+    @CommandLine.Option(
             names = {"--endpoint"},
             description = "GRPC interface of the node.",
             defaultValue = "http://localhost:20000")
     private String endpoint;
 
-    @Option(
-            names = {"--moduleRef"},
-            description = "Module Reference",
-            defaultValue = "247a7ac6efd2e46f72fd18741a6d1a0254ec14f95639df37079a576b2033873e")
-    private String moduleRef;
 
     @Override
     public Integer call() throws ClientInitializationException, MalformedURLException {
@@ -39,19 +32,20 @@ public class GetModuleSource implements Callable<Integer> {
                 .port(endpointUrl.getPort())
                 .build();
 
-        WasmModule moduleSource = ClientV2
+        Iterator<Summary> blockTransactionEvents = ClientV2
                 .from(connection)
-                .getModuleSource(
-                        BlockQuery.LAST_FINAL,
-                        ModuleRef.from(moduleRef));
+                .getBlockTransactionEvents(BlockQuery.LAST_FINAL);
 
-        System.out.println(moduleSource);
+        while(blockTransactionEvents.hasNext()) {
+            Summary event = blockTransactionEvents.next();
+            System.out.println(event);
+        }
 
         return 0;
     }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new GetModuleSource()).execute(args);
+        int exitCode = new CommandLine(new com.concordium.sdk.examples.GetBlockTransactionEvents()).execute(args);
         System.exit(exitCode);
     }
 }
