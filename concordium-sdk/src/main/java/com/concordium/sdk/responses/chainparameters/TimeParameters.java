40ed0cab3e843a34b5907bfabf37bcdd59687c3b
magnusbechwind <magnusbechwind@gmail.com>
2023-11-24 15:35:32 +0100

    Removed unnecessary classes and json parsing in package responses.chainparameters

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/chainparameters/TimeParameters.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/chainparameters/TimeParameters.java
@@ -1,12 +1,10 @@
 package com.concordium.sdk.responses.chainparameters;
 
 import com.concordium.grpc.v2.TimeParametersCpv1;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.extern.jackson.Jacksonized;
 
 /**
  * Time parameters exists from protocol version 4 and onwards.
@@ -15,13 +13,11 @@ import lombok.extern.jackson.Jacksonized;
 @ToString
 @EqualsAndHashCode
 @Builder
-@Jacksonized
 public final class TimeParameters {
 
     /**
      * Length of a reward period (a number of epochs).
      */
-    @JsonProperty("rewardPeriodLength")
     private final long rewardPeriodLength;
 
     /**
@@ -29,7 +25,6 @@ public final class TimeParameters {
      * Note. The Concordium node returns a floating point number with arbitrary precision so the
      * 'mintPerPayday' might be rounded (since a 'double' has a precision of 15 decimals).
      */
-    @JsonProperty("mintPerDay")
     private final double mintPerPayday;
 
     public static TimeParameters from(TimeParametersCpv1 update) {
