b476855046ca7356d6add6e0ee75c38d14d5c7e0
magnusbechwind <magnusbechwind@gmail.com>
2023-11-24 15:22:06 +0100

    Removed unnecessary json parsing in FinalizationData and Finalizer

--- concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/Finalizer.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/responses/blocksummary/Finalizer.java
@@ -1,8 +1,6 @@
 package com.concordium.sdk.responses.blocksummary;
 
 import com.concordium.sdk.responses.AccountIndex;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,14 +30,4 @@ public final class Finalizer {
      * Whether the finalizer's signature is present.
      */
     private final boolean signed;
-
-
-    @JsonCreator
-    Finalizer(@JsonProperty("bakerId") AccountIndex bakerId,
-              @JsonProperty("weight") BigInteger weight,
-              @JsonProperty("signed") boolean signed) {
-        this.bakerId = bakerId;
-        this.weight = weight;
-        this.signed = signed;
-    }
 }
