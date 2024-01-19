cc8acc1e76f759ff852b11a9122f94cc127baac3
orhoj <jo@concordium.com>
2024-01-19 14:31:07 +0100

    Add width and height to seed phrase input

--- concordium-android-sdk-examples/app/src/main/java/com/example/android_sdk_example/activities/SeedPhraseActivity.kt
+++ concordium-android-sdk-examples/app/src/main/java/com/example/android_sdk_example/activities/SeedPhraseActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,8 +16,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cash.z.ecc.android.bip39.Mnemonics.MnemonicCode
 import com.example.android_sdk_example.Storage
 import com.example.android_sdk_example.ui.Container
@@ -59,7 +63,8 @@ fun SeedPhraseView(initialPhrase: String, onSubmit: (phrase: String) -> Unit) {
             TextField(
                 value = phrase,
                 onValueChange = { phrase = it },
-                label = { Text("Enter your seed phrase") }
+                label = { Text("Enter your seed phrase") },
+                modifier = Modifier.fillMaxWidth().height(128.dp)
             )
             Button(onClick = { onSubmit(phrase) }, enabled = validPhrase) {
                 Text(text = "Submit")
