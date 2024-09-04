536076f746206ccf2d7d2944b0c6ec32f5c4ab5f
Oleg Koretsky <oko@concordium.com>
2024-09-04 13:08:55 +0300

    Remove encrypted transfers
    Removed previously deprecated encrypted transfers:
    EncryptedTransferTransaction, EncryptedTransferWithMemoTransaction,
    TransferToEncryptedTransaction

--- concordium-sdk/src/main/java/com/concordium/sdk/transactions/TransactionFactory.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/transactions/TransactionFactory.java
@@ -1,7 +1,6 @@
 package com.concordium.sdk.transactions;
 
 import com.concordium.sdk.crypto.bakertransactions.BakerKeys;
-import com.concordium.sdk.crypto.elgamal.ElgamalPublicKey;
 import com.concordium.sdk.crypto.elgamal.ElgamalSecretKey;
 import com.concordium.sdk.crypto.encryptedtransfers.EncryptedTransfers;
 import com.concordium.sdk.responses.accountinfo.AccountEncryptedAmount;
@@ -143,112 +142,6 @@ public class TransactionFactory {
                 .index(jniOutput.getIndex())
                 .proof(jniOutput.getProof())
                 .remainingAmount(jniOutput.getRemainingAmount());
-    }
-
-    /**
-     * Creates a new {@link TransferToEncryptedTransaction.TransferToEncryptedTransactionBuilder} for
-     * creating a {@link TransferToEncryptedTransaction}
-     *
-     * @return the builder for a {@link TransferToEncryptedTransaction}
-     * @deprecated Encrypted transfers are deprecated and partially removed since protocol version 7
-     */
-    @Deprecated
-    public static TransferToEncryptedTransaction.TransferToEncryptedTransactionBuilder newTransferToEncrypted() {
-        return TransferToEncryptedTransaction.builder();
-    }
-
-
-    /**
-     * Creates a new {@link EncryptedTransferTransaction.EncryptedTransferTransactionBuilder} for
-     * creating a {@link EncryptedTransferTransaction}
-     *
-     * @return the builder for a {@link EncryptedTransferTransaction}
-     * @deprecated Encrypted transfers are deprecated and partially removed since protocol version 7
-     */
-    @Deprecated
-    public static EncryptedTransferTransaction.EncryptedTransferTransactionBuilder newEncryptedTransfer() {
-        return EncryptedTransferTransaction.builder();
-    }
-
-
-    /**
-     * Creates a new {@link EncryptedTransferTransaction.EncryptedTransferTransactionBuilder} for
-     * creating a {@link EncryptedTransferTransaction}
-     *
-     * @return the builder for a {@link EncryptedTransferTransaction}
-     * @deprecated Encrypted transfers are deprecated and partially removed since protocol version 7
-     */
-    @Deprecated
-    public static EncryptedTransferTransaction.EncryptedTransferTransactionBuilder newEncryptedTransfer(
-            CryptographicParameters cryptographicParameters,
-            AccountEncryptedAmount accountEncryptedAmount,
-            ElgamalPublicKey receiverPublicKey,
-            ElgamalSecretKey senderSecretKey,
-            CCDAmount amountToSend) {
-        val jniOutput = EncryptedTransfers.createEncryptedTransferPayload(
-                cryptographicParameters,
-                accountEncryptedAmount,
-                receiverPublicKey,
-                senderSecretKey,
-                amountToSend
-        );
-
-        val amountTransferData = EncryptedAmountTransferData.builder()
-                .remainingAmount(jniOutput.getRemainingAmount())
-                .transferAmount(jniOutput.getTransferAmount())
-                .index(jniOutput.getIndex())
-                .proof(jniOutput.getProof()).build();
-
-        return EncryptedTransferTransaction
-                .builder()
-                .data(amountTransferData);
-
-    }
-
-    /**
-     * Creates a new {@link EncryptedTransferWithMemoTransaction.EncryptedTransferWithMemoTransactionBuilder} for
-     * creating a {@link EncryptedTransferWithMemoTransaction}
-     *
-     * @return the builder for a {@link EncryptedTransferWithMemoTransaction}
-     * @deprecated Encrypted transfers are deprecated and partially removed since protocol version 7
-     */
-    @Deprecated
-    public static EncryptedTransferWithMemoTransaction.EncryptedTransferWithMemoTransactionBuilder newEncryptedTransferWithMemo() {
-        return EncryptedTransferWithMemoTransaction.builder();
-    }
-
-    /**
-     * Creates a new {@link EncryptedTransferWithMemoTransaction.EncryptedTransferWithMemoTransactionBuilder} for
-     * creating a {@link EncryptedTransferWithMemoTransaction}
-     *
-     * @return the builder for a {@link EncryptedTransferWithMemoTransaction}
-     * @deprecated Encrypted transfers are deprecated and partially removed since protocol version 7
-     */
-    @Deprecated
-    public static EncryptedTransferWithMemoTransaction.EncryptedTransferWithMemoTransactionBuilder newEncryptedTransferWithMemo(
-            CryptographicParameters cryptographicParameters,
-            AccountEncryptedAmount accountEncryptedAmount,
-            ElgamalPublicKey receiverPublicKey,
-            ElgamalSecretKey senderSecretKey,
-            CCDAmount amountToSend) {
-        val jniOutput = EncryptedTransfers.createEncryptedTransferPayload(
-                cryptographicParameters,
-                accountEncryptedAmount,
-                receiverPublicKey,
-                senderSecretKey,
-                amountToSend
-        );
-
-        val amountTransferData = EncryptedAmountTransferData.builder()
-                .remainingAmount(jniOutput.getRemainingAmount())
-                .transferAmount(jniOutput.getTransferAmount())
-                .index(jniOutput.getIndex())
-                .proof(jniOutput.getProof()).build();
-
-        return EncryptedTransferWithMemoTransaction
-                .builder()
-                .data(amountTransferData);
-
     }
 
     /**
