878712728e4db9a9b2299c52493fdf919f930f20
Oleg Koretsky <oko@concordium.com>
2025-10-16 17:24:12 +0300

    Add token transfer tx cost test with memo

--- concordium-sdk/src/test/java/com/concordium/sdk/transactions/TokenUpdateTransactionTest.java
+++ concordium-sdk/src/test/java/com/concordium/sdk/transactions/TokenUpdateTransactionTest.java
@@ -134,4 +134,46 @@ public class TokenUpdateTransactionTest {
                         .getMaxEnergyCost()
         );
     }
+
+    @Test
+    @SneakyThrows
+    public void testTokenUpdateTransferTransactionCostWithMemo() {
+        Assert.assertEquals(
+                UInt64.from("828"),
+                TransactionFactory
+                        .newTokenUpdate()
+                        .sender(AccountAddress.from("3JwD2Wm3nMbsowCwb1iGEpnt47UQgdrtnq2qT6opJc3z2AgCrc"))
+                        .payload(
+                                TokenUpdate
+                                        .builder()
+                                        .tokenSymbol("TEST")
+                                        .operation(
+                                                TransferTokenOperation
+                                                        .builder()
+                                                        .amount(
+                                                                new TokenOperationAmount(
+                                                                        new BigDecimal("1.5"),
+                                                                        2
+                                                                )
+                                                        )
+                                                        .recipient(
+                                                                new TaggedTokenHolderAccount(
+                                                                        AccountAddress.from("3hYXYEPuGyhFcVRhSk2cVgKBhzVcAryjPskYk4SecpwGnoHhuM")
+                                                                )
+                                                        )
+                                                        .memo(
+                                                                CborMemo.from("Memo for the test, long memo, quite a long text for a memo if you ask me")
+                                                        )
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .nonce(Nonce.from(78910))
+                        .expiry(Expiry.from(123456))
+                        .signer(TransactionTestHelper.getValidSigner())
+                        .build()
+                        .getHeader()
+                        .getMaxEnergyCost()
+        );
+    }
 }
