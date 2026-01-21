64ec315bcd73a8c976554b6e68ff0f259779ddc5
Oleg Koretsky <oko@concordium.com>
2026-01-21 14:50:47 +0200

    Fix incorrect prefix length for v1 signatures

--- concordium-sdk/src/test/java/com/concordium/sdk/transactions/PartiallySignedSponsoredTransactionTest.java
+++ concordium-sdk/src/test/java/com/concordium/sdk/transactions/PartiallySignedSponsoredTransactionTest.java
@@ -21,11 +21,13 @@ public class PartiallySignedSponsoredTransactionTest {
             SignerEntry.from(Index.from(0), Index.from(0),
                     ED25519SecretKey.from("cd20ea0127cddf77cf2c20a18ec4516a99528a72e642ac7deb92131a9d108ae9"))
     );
-    private final String COMPLETE_TX_HASH = "5eb451836222ac9b68974fa50a06dfc36bd795e3ee832b61681d7adb887a60d8";
+    private final String SIGNED_BY_SENDER_JSON = "{\"senderSignature\":\"01000100004099ecf806f9dbcbba1f7dafa41f029cc342fd630b8ae23f047e87c1e0b7c271a73a5d0a6b6071ab72070055964de66a7bd1b1d417098537cfd4d8920a3c8afd0d\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
+    private final String SIGNED_BY_SPONSOR_JSON = "{\"sponsorSignature\":\"0100010000400005d925b3e8cb69693e6940e110004c0c94fb70f94d2cb43b73a2b2a6b935ce81bf0f500fdbffedd5f852be7c0dbca80191fd9ea596cc1875eec7024803190b\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
+    private final String COMPLETE_TX_HASH = "68e31a4ef7bd0f53e5ede99e62a9d702981091a0a86097110df70f56a4b2de57";
 
     @Test
     @SneakyThrows
-    public void serializationSignedBySender() {
+    public void creationBySender() {
         val transaction = TransactionFactory
                 .newPayloadSubmission(new RawPayload(new byte[]{0, 1, 2, 3}), UInt64.from(300))
                 .sender(SENDER)
@@ -33,21 +35,19 @@ public class PartiallySignedSponsoredTransactionTest {
                 .expiry(Expiry.from(123456))
                 .sponsoredBy(SPONSOR)
                 .signAsSender(SENDER_SIGNER);
-        val expectedJson = "{\"senderSignature\":\"010001000040c61f531faaeef7e4d461076fdf7e1df6db48fbf00b1cebab23f3f42e9a84a40a32d44d656e7fce40c8a7a1bc9fdf0e69e2b9cdba78e1dc5a469d1412a724b30e\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
-
         Assert.assertEquals(
-                expectedJson,
+                SIGNED_BY_SENDER_JSON,
                 JsonMapper.INSTANCE.writeValueAsString(transaction)
         );
         Assert.assertEquals(
                 transaction,
-                JsonMapper.INSTANCE.readValue(expectedJson, PartiallySignedSponsoredTransaction.class)
+                JsonMapper.INSTANCE.readValue(SIGNED_BY_SENDER_JSON, PartiallySignedSponsoredTransaction.class)
         );
     }
 
     @Test
     @SneakyThrows
-    public void serializationSignedBySponsor() {
+    public void creationBySponsor() {
         val transaction = TransactionFactory
                 .newPayloadSubmission(new RawPayload(new byte[]{0, 1, 2, 3}), UInt64.from(300))
                 .sender(SENDER)
@@ -55,23 +55,20 @@ public class PartiallySignedSponsoredTransactionTest {
                 .expiry(Expiry.from(123456))
                 .sponsoredBy(SPONSOR)
                 .signAsSponsor(SPONSOR_SIGNER);
-        val expectedJson = "{\"sponsorSignature\":\"0100010000401663d5cfd87beb9bd6a90f37251d616a08d03dc9ba26d26776c11227e75d95a3524602e05c5aecef01dc08c282e5db383fabcabcc3b4bd134a7f11c8d3ef2b09\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
-
         Assert.assertEquals(
-                expectedJson,
+                SIGNED_BY_SPONSOR_JSON,
                 JsonMapper.INSTANCE.writeValueAsString(transaction)
         );
         Assert.assertEquals(
                 transaction,
-                JsonMapper.INSTANCE.readValue(expectedJson, PartiallySignedSponsoredTransaction.class)
+                JsonMapper.INSTANCE.readValue(SIGNED_BY_SPONSOR_JSON, PartiallySignedSponsoredTransaction.class)
         );
     }
 
     @Test
     @SneakyThrows
     public void completionBySponsor() {
-        val signedBySenderJson = "{\"senderSignature\":\"010001000040c61f531faaeef7e4d461076fdf7e1df6db48fbf00b1cebab23f3f42e9a84a40a32d44d656e7fce40c8a7a1bc9fdf0e69e2b9cdba78e1dc5a469d1412a724b30e\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
-        val signedBySenderTransaction = JsonMapper.INSTANCE.readValue(signedBySenderJson, PartiallySignedSponsoredTransaction.class);
+        val signedBySenderTransaction = JsonMapper.INSTANCE.readValue(SIGNED_BY_SENDER_JSON, PartiallySignedSponsoredTransaction.class);
         val completeTransaction = TransactionFactory
                 .completeSponsoredTransaction(signedBySenderTransaction)
                 .asSponsor(SPONSOR_SIGNER);
@@ -85,8 +82,7 @@ public class PartiallySignedSponsoredTransactionTest {
     @Test
     @SneakyThrows
     public void completionBySender() {
-        val signedBySponsorJson = "{\"sponsorSignature\":\"0100010000401663d5cfd87beb9bd6a90f37251d616a08d03dc9ba26d26776c11227e75d95a3524602e05c5aecef01dc08c282e5db383fabcabcc3b4bd134a7f11c8d3ef2b09\",\"header\":\"0001301d6b1710b5735afc24589801213d13aa6b4478890fdfe8195bca0eaf22614e000000000001343e000000000000025600000004000000000001e240637388bcd6cccb39c324bb96265262cadb806d884b36e8541fdd12f6a1c10ee1\",\"payload\":\"00010203\"}";
-        val signedBySponsorTransaction = JsonMapper.INSTANCE.readValue(signedBySponsorJson, PartiallySignedSponsoredTransaction.class);
+        val signedBySponsorTransaction = JsonMapper.INSTANCE.readValue(SIGNED_BY_SPONSOR_JSON, PartiallySignedSponsoredTransaction.class);
         val completeTransaction = TransactionFactory
                 .completeSponsoredTransaction(signedBySponsorTransaction)
                 .asSender(SENDER_SIGNER);
