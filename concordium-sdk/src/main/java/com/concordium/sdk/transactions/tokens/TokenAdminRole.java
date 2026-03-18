e54fe482961a5f7b01b41a63cfec95ee62f03d7f
Oleg Koretsky <oko@concordium.com>
2026-03-18 13:42:40 +0200

    Add update metadata token admin role, add javadoc

--- concordium-sdk/src/main/java/com/concordium/sdk/transactions/tokens/TokenAdminRole.java
+++ concordium-sdk/src/main/java/com/concordium/sdk/transactions/tokens/TokenAdminRole.java
@@ -7,12 +7,40 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import java.util.List;
 
 public enum TokenAdminRole {
+    /**
+     * Gives authority to perform token-assign-admin-roles and token-revoke-admin-roles operations.
+     */
     UPDATE_ADMIN_ROLES("updateAdminRoles"),
+
+    /**
+     * Gives authority to perform token-mint operations.
+     */
     MINT("mint"),
+
+    /**
+     * Gives authority to perform token-burn operations.
+     */
     BURN("burn"),
+
+    /**
+     * Gives authority to perform token-add-allow-list and token-remove-allow-list operations.
+     */
     UPDATE_ALLOW_LIST("updateAllowList"),
+
+    /**
+     * Gives authority to perform token-add-deny-list and token-remove-deny-list operations.
+     */
     UPDATE_DENY_LIST("updateDenyList"),
+
+    /**
+     * Gives authority to perform token-pause and token-unpause operations.
+     */
     PAUSE("pause"),
+
+    /**
+     * Gives authority to perform token-update-metadata operations.
+     */
+    UPDATE_METADATA("updateMetadata")
     ;
 
     private final String identifier;
